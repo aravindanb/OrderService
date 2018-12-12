fd0a45f18b0ccb300b40e103e9cab8f96bb63581
aravin <aravindannair24@gmail.com>
2018-12-12 17:38:48 -0500

    adds a new /orders endpoint
    returns mock data at the moment

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.get("/orders", (req,res,next)=>{
+    res.json(["Tony", "Lisa", "Micahel", "Ginger", "Food"]);
+});
+
 app.listen(3000, ()=>{
     console.log("Order Service Running on port 3000")
 });
