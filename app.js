3ac921cc506a7cbe66a8b61a8e3458879e4fe35f
aravin <aravindannair24@gmail.com>
2018-12-12 17:54:45 -0500

    Adds MongoDB, Body Parser and Nodemon dependencies
    Still returns mock data

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
-var express = require("express");
+var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var bodyParser = require('body-parser')
 var app = express();
 
 app.get("/orders", (req,res,next)=>{
-    res.json(["Tony", "Lisa", "Micahel", "Ginger", "Food"]);
+    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
 app.listen(3000, ()=>{
