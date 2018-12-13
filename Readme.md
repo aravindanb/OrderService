143aad5fa768dc0817565bf9eaa23633cca1ff63
aravin <aravindannair24@gmail.com>
2018-12-13 17:21:17 -0500

    Connect to a MongoDB
    - add DB connection configuration to config/db.js
    - use it in app.js
    -  Move the route import and Server Listening within the success the callback of Mongo Successful connection

--- Readme.md
+++ Readme.md
@@ -6,6 +6,23 @@
 - Dev server in watch mode `npm run dev`  or
 - `node app.js`
 
+### Database account
+
+```
+user : AravindanB
+password : 
+```
+- To connect using the mongo shell:
+
+  ` mongo ds149682.mlab.com:49682/aravindan-orderservice-node -u <dbuser> -p <dbpassword> `
+
+- To connect using a driver via the standard MongoDB URI 
+
+  `mongodb://<dbuser>:<dbpassword>@ds149682.mlab.com:49682/aravindan-orderservice-node`
+
+
+
+
 ### Orders Endpoints
 
 - POST /orders 
--- app.js
+++ app.js
@@ -1,19 +1,27 @@
-var express = require('express');
-var MongoClient = require('mongodb').MongoClient;
-var bodyParser = require('body-parser');
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./config/db');
 const app = express();
 const port = 3000;
 
 //user bodyParser to process URL encoded forms
 app.use(bodyParser.urlencoded({ extended: true}));
 
-//import the routes in the app.js
-require('./app/routes')(app,{});
+MongoClient.connect(db.url, (err, database)=>{
+    if(err){
+        return console.log(err);
+    }
+    //import the routes in the app.js
+    require('./app/routes')(app,{});
+    /**
+    * Server Listening at 3000
+    */
+    app.listen(port, ()=>{
+        console.log("Order Service Running on port 3000")
+    });
+})
 
 
-/**
- * Server Listening at 3000
- */
-app.listen(port, ()=>{
-    console.log("Order Service Running on port 3000")
-});
+
+
