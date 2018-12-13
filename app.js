e42de60869dce0b99c71461c655db77bef376c0b
aravin <aravindannair24@gmail.com>
2018-12-13 15:22:02 -0500

    import the routes in the app.js

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
-var bodyParser = require('body-parser')
-var app = express();
-
+var bodyParser = require('body-parser');
+const app = express();
 const port = 3000;
+
+//import the routes in the app.js
+require('./app/routes')(app,{});
+
+/**
+ * Endpoints for the Order Service
+ */
+
+// GET /orders
 app.get("/orders", (req,res,next)=>{
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
+/**
+ * Server Listening at 3000
+ */
 app.listen(port, ()=>{
     console.log("Order Service Running on port 3000")
 });
