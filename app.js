a6d3b3c1fb655dd461b25e0892d20d11444af832
aravin <aravindannair24@gmail.com>
2018-12-13 15:15:02 -0500

    use the const port

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser')
 var app = express();
 
+const port = 3000;
 app.get("/orders", (req,res,next)=>{
     res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
-app.listen(3000, ()=>{
+app.listen(port, ()=>{
     console.log("Order Service Running on port 3000")
 });
