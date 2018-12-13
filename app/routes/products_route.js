e00946dd3af3a119749e112eef939411c764d313
aravin <aravindannair24@gmail.com>
2018-12-13 15:48:55 -0500

    GET (all) Products Endpoint

--- app/routes/products_route.js
+++ app/routes/products_route.js
@@ -1,4 +1,10 @@
 //configure routes for product API's here
 
+const productsEndpoint = '/products';
 module.exports = function(app, db) {
+
+// GET /products
+app.get(productsEndpoint, (req,res,next)=>{
+    res.json(["Caprina", "Nivea", "Pears", "Irish Spring", "Yves Rocher"]);
+});
 };
